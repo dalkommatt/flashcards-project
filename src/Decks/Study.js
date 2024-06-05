e9ab985eff136bc931a4a78e80114972d7b223cc
Matt <mr.matthewc.king@gmail.com>
2024-06-05 14:18:32 -0600

    Update Study.js

--- src/Decks/Study.js
+++ src/Decks/Study.js
@@ -9,14 +9,15 @@ export default function Study({ decks }) {
   const deck = decks.find((deck) => deck.id === parseInt(deckId));
   const [cardIndex, setCardIndex] = useState(0);
   const [flipped, setFlipped] = useState(false);
-  const [card, setCard] = useState();
+  const [cards, setCards] = useState();
+  const card = cards ? cards[cardIndex] : null;
 
   useEffect(() => {
     const abortController = new AbortController();
     readDeck(deckId, abortController.signal) // fetch the entire deck
       .then((deck) => {
         if (deck.cards) {
-          setCard(deck.cards[cardIndex]);
+          setCards(deck.cards);
         }
       }); // select the card by its index
     return () => abortController.abort();
@@ -35,56 +36,66 @@ export default function Study({ decks }) {
               <Link to={`/decks/${deckId}`}>{deck.name}</Link>
             </li>
             <li className="breadcrumb-item active" aria-current="page">
-              Create Deck
+              Study Deck
             </li>
           </ol>
         </nav>
         <h1 className="text-5xl font-semibold">Study: {deck.name}</h1>
-        <div className="border border-1 rounded p-6 min-h-60 shadow flex flex-col justify-between px-3 mt-4">
-          <div>
-            <div className="flex flex-row justify-between w-full px-3">
-              <h3 className="text-2xl">
-                Card {cardIndex + 1} of {deck.cards.length}
-              </h3>
-              {/*  <p>{deck[cardIndex].length} cards</p> */}
+        {cards.length < 3 ? (
+          <>
+            <h3 className="h3 my-4">Not enough cards</h3>
+            <p>
+              You need at least 3 card to study. There are {cards.length} cards
+              in this deck.
+            </p>
+          </>
+        ) : (
+          <div className="border border-1 rounded p-6 min-h-60 shadow flex flex-col justify-between px-3 mt-4">
+            <div>
+              <div className="flex flex-row justify-between w-full px-3">
+                <h3 className="text-2xl">
+                  Card {cardIndex + 1} of {deck.cards.length}
+                </h3>
+                {/*  <p>{deck[cardIndex].length} cards</p> */}
+              </div>
+              <div className="flex text-lg pt-2 px-3">
+                {flipped ? card?.back : card?.front}
+              </div>
             </div>
-            <div className="flex text-lg pt-2 px-3">
-              {flipped ? card?.back : card?.front}
-            </div>
-          </div>
-          <div className="flex flex-row w-full px-3 justify-between">
-            <div className="space-x-2">
-              <button
-                onClick={() => {
-                  setFlipped((current) => !current);
-                }}
-                className="btn btn-secondary"
-              >
-                Flip
-              </button>
-              {flipped && (
+            <div className="flex flex-row w-full px-3 justify-between">
+              <div className="space-x-2">
                 <button
                   onClick={() => {
-                    if (cardIndex === deck.cards.length - 1) {
-                      if (window.confirm("Restart cards?")) {
-                        setCardIndex(0);
-                      } else {
-                        // push to home screen
-                        navigate("/");
-                      }
-                    } else {
-                      setCardIndex((current) => current + 1);
-                      setFlipped(false);
-                    }
+                    setFlipped((current) => !current);
                   }}
-                  className="btn btn-primary"
+                  className="btn btn-secondary"
                 >
-                  Next
+                  Flip
                 </button>
-              )}
+                {flipped && (
+                  <button
+                    onClick={() => {
+                      if (cardIndex === deck.cards.length - 1) {
+                        if (window.confirm("Restart cards?")) {
+                          setCardIndex(0);
+                        } else {
+                          // push to home screen
+                          navigate("/");
+                        }
+                      } else {
+                        setCardIndex((current) => current + 1);
+                        setFlipped(false);
+                      }
+                    }}
+                    className="btn btn-primary"
+                  >
+                    Next
+                  </button>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
